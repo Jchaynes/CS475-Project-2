07f3dfb8a90f0eb995af139ae9e74e59cfa5d8a0
Jon Haynes <jonathan.c.haynes@gmail.com>
2021-09-29 16:09:23 -0700

    refactored to more functional program

--- encryption-server.py
+++ encryption-server.py
@@ -1,16 +1,29 @@
-from socket import SOCK_STREAM, AF_INET, socket, MSG_WAITALL
+from socket import SOCK_STREAM, AF_INET, socket, MSG_WAITALL,htons,ntohs
 import argparse
 import sys
 import threading
+from typing import ByteString
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES, PKCS1_OAEP
 from Crypto.Random import get_random_bytes
 
 
+
+  # 1. Server listens on port
+  # 2. Client connects to port
+  # 3. Client sends public RSA key (4 bytes unsigned)
+  # 4. Server generates AES session key (16 bytes unsigned)
+  # 5. Server encrypts AES key with RSA key
+  # 6. Server sends encrypted key to client and waits
+  # 7. Client decrypts session key
+  # 8. Client encrypts either hardcoded string or user input with session key (Send length first, up to 4 bytes unsigned)
+  # 9. Client sends message to confirm receipt and decryption of key (Send length first, up to 4 bytes unsigned)
+  # 10. Server decrypts message from client with session key
+  # 11. Server sends hardcoded or user input message encrypted with session key
+
 KEYSIZE = 16
 
 parser = argparse.ArgumentParser()
-#parser.add_argument("-h","--help",help="-s <server> -p <port>")
 parser.add_argument("-a","--address",help="interface address to listen on",required="true")
 parser.add_argument("-p","--port",help="listening port",required="true")
 args = parser.parse_args()
@@ -28,69 +41,60 @@ class cryptoClient:
     keyLen = self.socket.recv(4,MSG_WAITALL)
     return keyLen
   
+  def send(self,message : bytes):
+    msgLen = sys.getsizeof(message)#.to_bytes(4,'little')
+    self.socket.send(msgLen.to_bytes(4,sys.byteorder))
+    self.socket.send(message)
+
+
+clients = [cryptoClient]
 
 def create_socket(address,port):
   skt = socket(AF_INET,SOCK_STREAM)
   skt.bind(('0.0.0.0', int(args.port)))
   return skt
 
+def decrypt_data(session_key,nonce,tag, ciphertext):
+  cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+  data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+  return  data.decode("utf-8")
+
 def encrypt_data(session_key, plaintext):
-  print("Encrypting data with encrypted session key")
+  print("Encrypting data with session key")
   #Encrypt the data with AES EAX mode
   cipher_aes = AES.new(session_key,AES.MODE_EAX)
   ciphertext, tag = cipher_aes.encrypt_and_digest(plaintext)
-  
-  file_out = open("encrypted_data.bin", "wb")
-  [ file_out.write(x) for x in (session_key, cipher_aes.nonce, tag, ciphertext)]
-  file_out.close()
-  #write_chunks = [session_key,cipher_aes.nonce,tag,ciphertext]
-  #return write_chunks
 
-clients = [cryptoClient]
+  chunks = [cipher_aes.nonce,tag,ciphertext]
+  data = b''.join(chunks)
+  nonce_len = sys.getsizeof(cipher_aes.nonce)
+  tag_len = sys.getsizeof(tag)
+  ciphertext_len = sys.getsizeof(ciphertext)
+  print("Message size is:", data, sys.getsizeof(data))
+
+  print("Nonce: ", cipher_aes.nonce, nonce_len)
+  print("Tag: ", tag,  tag_len)
+  print("Ciphertext: ", ciphertext, ciphertext_len)
+  return data
+
+def parse_encrypted_msg(enc_msg : bytes):
+  if(len(enc_msg < 33)):
+    raise OSError
+  nonce = enc_msg[0:16]
+  tag = enc_msg[16:32]
+  ciphertext = enc_msg[32:]
+  return (nonce,tag,ciphertext)
 
 def listen(skt:socket):
   print("Listening for connections on ", skt)
-  while(True):
-    con_socket, address = skt.accept()
-    print("Accepted connection from ", socket, address)
-    
-    #Read in Key Length for client RSA key
-    client = cryptoClient(con_socket)
-    keyLen = client.read_msg_len()
-    msg = con_socket.recv(int.from_bytes(keyLen,'little'))
-
-    #Format the read in key to proper RSA key object format.
-    try:
-      recipient_key = RSA.import_key(msg)
-    except:
-      print("Error converting RSA key from client: {0}".format(sys.exc_info[0]))
-      raise
-
-    #print("RSA key is: " , recipient_key.export_key().decode("utf-8"))
-    print("Session Key is: ", client.session_key)
-
-    #Encrypt the AES key with the user's public RSA key
-    cipher_rsa = PKCS1_OAEP.new(recipient_key)
-    enc_session_key = cipher_rsa.encrypt(client.session_key)
-
-    #Send the encrypted session Key back to client.
-    con_socket.send(sys.getsizeof(enc_session_key).to_bytes(4,'little'))
-    con_socket.send(enc_session_key)
-    
-    #client.socket.send(sys.getsizeof(encrypted_data).to_bytes(4,'little'))
-
-    #client.socket.send(encrypted_data)
-    #TODO: Build input loop and remove socket.close()
-    client.socket.close()
-    return
-    #Send the client the encrypted session key, nonce, tag, and their intial string.
-
-def read_client(con_socket:socket, address):
+  #2. Client connects to port
+  con_socket, address = skt.accept()
+  print("Accepted connection from ", con_socket, address)
   
   #Read in Key Length for client RSA key
-  keyLen = con_socket.recv(4,MSG_WAITALL)
-  
-  #Read in client RSA key bytes in little endian format
+  client = cryptoClient(con_socket)
+  keyLen = client.read_msg_len()
+  #3. Recieve RSA Key
   msg = con_socket.recv(int.from_bytes(keyLen,'little'))
 
   #Format the read in key to proper RSA key object format.
@@ -100,28 +104,64 @@ def read_client(con_socket:socket, address):
     print("Error converting RSA key from client: {0}".format(sys.exc_info[0]))
     raise
 
-  #Generate Random AES Key
-  session_key = get_random_bytes(16)
-
-  #Encrypt the AES key with the user's public RSA key
+  #5. Encrypt the AES key with the user's public RSA key
   cipher_rsa = PKCS1_OAEP.new(recipient_key)
-  enc_session_key = cipher_rsa.encrypt(session_key)
+  enc_session_key = cipher_rsa.encrypt(client.session_key)
+
+  #6. Send the encrypted session Key back to client.
+  con_socket.send(sys.getsizeof(enc_session_key).to_bytes(4,'little'))
+  con_socket.send(enc_session_key)
+
+  #9. Read message from client.
+  client_msg_len = client.socket.recv(4, MSG_WAITALL)
+  client_msg = client.socket.recv(client_msg_len)
   
+  #10. Decrypt the message with AES session key
+  nonce,tag,ciphertext = parse_encrypted_msg(client_msg)
+  decrypted_client_msg = decrypt_data(client.session_key,nonce,tag,ciphertext)
+  print(decrypted_client_msg)
 
 
-  file_in = open("encrypted_data.bin",'rb')
-  encrypted_data = file_in.read()
-  #encrypted_data = encrypt_data(enc_session_key,data)
-
+  encrypted_data = encrypt_data(client.session_key,data)
+  client.send(encrypted_data)
+  client.socket.close()
+  return
   #Send the client the encrypted session key, nonce, tag, and their intial string.
 
+def read_client(client:cryptoClient):
+  # while(True):
+    #Read in Key Length for client RSA key
+    print("reading data from the client...")
+    keyLen = client.socket.recv(4,MSG_WAITALL)
+    print("Reading in data length of %d " % (int.from_bytes(keyLen,'little')))
+    if(int.from_bytes(keyLen,'little') > 0):      #Read in client RSA key bytes in little endian format
+      msg = client.socket.recv(int.from_bytes(keyLen,'little'))
+      print(msg)
+      nonce = msg[:16]
+      tag = msg[16:32]
+      ciphertext = msg[32:]
+      
+    
+      print(decrypt_data(client.session_key,nonce,tag,ciphertext))
+    return
+
+
+def send_client(client:cryptoClient):
+  # while(True):
+    print("Beggining chat with client.")
+    encrypted_string = encrypt_data(client.session_key,data)
+    client.send(encrypted_string)
+    userString = input("Chat: ")
+    if(userString.lower() == "quit"):
+      return
+    encrypted_string = encrypt_data(client.session_key,userString)
+    client.send(encrypted_string)
+  
 
 
 if __name__ == "__main__":
 
   srv_socket = create_socket(args.address,args.port)
   srv_socket.listen(2)
-
-  t1 = threading.Thread(target=listen(srv_socket))
-  t1.start()
-  t1.join()
+  #while(True):
+  listen(srv_socket)
